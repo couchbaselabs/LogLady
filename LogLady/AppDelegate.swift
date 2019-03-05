f5c8f58bcc71e0ce0ed724666f715af009c0369a
Jens Alfke <jens@couchbase.com>
2019-03-05 11:52:32 -0800

    Initial Commit

---
+++ LogLady/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LogLady
+//
+//  Created by Jens Alfke on 3/5/19.
+//  Copyright © 2019 Couchbase. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
